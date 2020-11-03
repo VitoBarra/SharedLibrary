787dd2988d9f29df588904b17d442ed9c3e87fb5
Vito Barra <vb.vitobarra@gmail.com>
2020-11-03 04:19:41 +0100

    MySqlTool Update

---
+++ GeneralUse.MySqlTool/MySqlReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SharedLibrary.GeneralUse.MySqlTool
+{
+
+
+    class MySqlReader
+    {
+        MySqlConnection Connection;
+        MySqlCommand Command;
+        public MySqlDataReader DataReader;
+
+
+
+        public MySqlReader(string SqlConnStr)
+        {
+            Connection = new MySqlConnection(SqlConnStr);
+            OpenConnection();
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                Connection.Open();
+            }
+            catch
+            {
+            }
+        }
+
+        public void ReadFrom(string colonne, string nomeTabella, string whereInfo = null)
+        {
+            if (Connection.State == ConnectionState.Open)
+            {
+                if (string.IsNullOrEmpty(whereInfo))
+                    Command = new MySqlCommand("SELECT " + colonne + "  FROM " + nomeTabella, Connection);
+                else
+                    Command = new MySqlCommand("SELECT " + colonne + "  FROM " + nomeTabella + " WHERE " + whereInfo, Connection);
+                DataReader = Command.ExecuteReader();
+            }
+        }
+    }
+
+
+    
+}
+
---
+++ GeneralUse.MySqlTool/MySqlWriter.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SharedLibrary.GeneralUse.MySqlTool
+{
+    class MySqlWriter
+    {
+        MySqlConnection Connection;
+        MySqlCommand Command;
+
+        public MySqlWriter(string SqlConnStr)
+        {
+            Connection = new MySqlConnection(SqlConnStr);
+
+            OpenConnection();
+        }
+
+
+        public void OpenConnection()
+        {
+            try
+            {
+                Connection.Open();
+            }
+            catch
+            {
+            }
+        }
+
+
+        public void ExecuteWriteCommand(string sqlCommand)
+        {
+            if (Connection.State == ConnectionState.Open)
+            {
+                Command = new MySqlCommand(sqlCommand, Connection);
+                Command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
