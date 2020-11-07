2d3741b5198c3aba80212cd0d655f8291f21d79d
Vito Barra <vb.vitobarra@gmail.com>
2020-11-07 07:38:30 +0100

    RestService

---
+++ GeneralUse.RestService/RestService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.GeneralUse.RestService
+{
+    public static class RestService
+    {
+        public static HttpClient client { get; private set; } = new HttpClient();
+
+
+        public static async Task<T> Service<T>(string EndPoint)
+        {
+            T Data = default(T);
+
+
+            HttpResponseMessage response = await client.GetAsync(EndPoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Data = JsonConvert.DeserializeObject<T>(content);
+            }
+
+
+
+            return Data;
+        }
+    }
+
+}
